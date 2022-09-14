fea519252f66d5f91617a2f03d161b946ddf367f
“Juan <juanenderica@hotmail.com>
2022-09-14 03:26:09 -0500

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+
+const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    'One Punch',
+    'Samurai X',
+    'Dragon Ball',
+  ]);
+
+  return (
+    <div>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr></hr>
+
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+    </div>
+  );
+};
+
+export default GifExpertApp;
